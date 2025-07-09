1643122b0e6ace6ecead4a1aa861dec29a44c962
Mitali Agrawal <mitaliagrawal012@gmail.com>
2025-07-09 22:17:38 +0530

    Update settings.py

--- sankalpSetu/settings.py
+++ sankalpSetu/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,3 +154,4 @@ REST_FRAMEWORK = {
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
