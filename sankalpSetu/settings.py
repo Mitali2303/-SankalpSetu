7fc36da01ea82b79d4907ac338e8c1437e880922
Mitali Agrawal <mitaliagrawal012@gmail.com>
2025-07-09 22:00:11 +0530

    Update settings.py

--- sankalpSetu/settings.py
+++ sankalpSetu/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-yh*hc$(7+0i(j^an$6*huh*s6ysyc1)-n1x7%w+m42k@0-bmf$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
